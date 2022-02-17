a6dc7d42856bd5d3b6e85630df1b9c2aef40e3d8
manali mohod <96103734+Manaliarunmohod@users.noreply.github.com>
2022-02-17 13:49:57 +0530

    pagination and email

---
+++ unit-3/sprint-1/day-1/assignments/src/configs/mail.js
@@ -0,0 +1,12 @@
+ 
+const nodemailer = require("nodemailer");
+// create reusable transporter object using the default SMTP transport
+module.exports = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "e7eca6d2defa91", // generated ethereal user
+      pass: "708dd2dd5bf07f", // generated ethereal password
+    },
+  });
