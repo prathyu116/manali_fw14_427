5d02799ab02fe65321d9850ee855e972a7f70f5e
Manali mohod <masaimanali@gmail.com>
2022-03-04 10:26:14 +0530

    commit

--- new folder async unit 4/sprint1/day2/assignment/index.js
+++ new folder async unit 4/sprint1/day2/assignment/index.js
@@ -2,12 +2,44 @@ const express=require('express')
 
 const app=express();
 
-
+app.use(express.json());
 
 app.get("",(req,res)=>{
 
     return res.send ("Hello");
 });
+// app.post("/users",async(req,res)=>{
+// try{
+// const book=await Book.create(req.body)
+// return res.send(book)
+
+
+
+// }catch(err){
+// return res.send(err.message)
+
+// }
+
+
+
+// })
+
+
+app.get("/users",async(req,res)=>{
+try{
+
+return res.send([{name:"manali",price:"500"},{name:"manali678",price:"500"},{name:"mnali",price:"50"},{name:"manali2",price:"509"}])
+
+
+
+}catch(err){
+return res.send(err.message)
+
+}
+
+
+
+})
 
 
 
