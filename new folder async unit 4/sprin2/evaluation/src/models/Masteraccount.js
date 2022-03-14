ce0188c5cf72fe63638edf4becc821fe6303c51c
Manali mohod <masaimanali@gmail.com>
2022-03-14 13:22:03 +0530

    changes

--- new folder async unit 4/sprin2/evaluation/src/models/Masteraccount.js
+++ new folder async unit 4/sprin2/evaluation/src/models/Masteraccount.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const masteraccountSchema= new mongoose.Schema({//change
     balance:{type:Number, required: true},
+    user_id:{type:mongoose.Schema.Types.ObjectId,ref:"users",required:true}
     
 
 },{
