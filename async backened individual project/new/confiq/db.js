b65c13ea079c104f5b6bb5edbba49ba4b446ae2f
Manali mohod <masaimanali@gmail.com>
2022-03-27 16:57:00 +0530

    second commit

--- async backened individual project/new/confiq/db.js
+++ async backened individual project/new/confiq/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = () => {
-    return mongoose.connect("mongodb+srv://mana:mana_123@cluster0.duupc.mongodb.net/fw41423")
+    return mongoose.connect("mongodb+srv://mana:mana_123@cluster0.duupc.mongodb.net/evaluationassignment")
    
    
     //mongodb+srv://neeraj:neeraj_123@cluster0.lbyiw.mongodb.net/freshmenu")
