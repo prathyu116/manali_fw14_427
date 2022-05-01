c028e2fdaac98f82eb6c3416b36b9cc5fc5c64f8
Manali mohod <masaimanali@gmail.com>
2022-05-01 12:33:11 +0530

    changes

---
+++ unit 5/assignment/router12/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+// import {Navbar} from "./Components/navbar/navbar";
+import {AllRoutes} from "./Routes/Routes";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ unit 5/assignment/router12/src/Components/about/about.jsx
@@ -0,0 +1,18 @@
+export const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+        <div>
+        An incredible eye for what's next in fashion. A passionate drive to exceed expectations. For more than 100 years, we've worked to deliver the best possible shopping experience, helping our customers express their style—not just buy fashion.
+
+Nordstrom, Inc. is a leading fashion retailer offering compelling clothing, shoes and accessories for men, women and kids. Since 1901, we've been committed to providing our customers with the best possible service—and to improving it every day.
+
+This commitment has taken us from a small Seattle shoe shop to the leading fashion retailer we are today. We're proud to serve customers in store at more than 350 Nordstrom, Nordstrom Local and Nordstrom Rack locations. We also serve customers online through our Nordstrom and Nordstrom Rack apps and websites.
+
+We believe fashion is a business of optimism, and in that spirit we continue to grow and evolve. Free shipping and free returns, mobile shopping and exciting new retail partnerships offer us continued opportunities to serve more customers in more ways with a fresh, relevant shopping experience and inspiring style. Fashion changes. Shopping changes. Our commitment to happy customers doesn't.
+
+Nordstrom, Inc. (JWN) is publicly traded on the NYSE.
+        </div>
+        </div>
+    )
+}
